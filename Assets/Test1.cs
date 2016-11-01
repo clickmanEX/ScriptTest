5b116cf922765e2b18f2dee477600accef1f322d
福田　淳 <clickmanver1.01@gmail.com>
2016-11-02 01:48:27 +0900

    ScriptTest プロジェクト　C#スクリプト Test1,、Test2を追加
    Test1に課題、Test2に発展課題のスクリプトを作成

---
+++ Assets/Test1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int[] array = { 10, 5, 32, 40, 1 };
+
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for(int j = 4; j >= 0; j--)
+        {
+            Debug.Log(array[j]);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sorcerer
+{
+    private int mp = 53;
+    public void Magic()
+    {
+        if(this.mp > 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした！残りMPは" + mp + "。");
+        }else
+        {
+            Debug.Log("MPが足りないため魔法は使えない。");
+        }
+    }
+}
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Sorcerer sorcerer = new Sorcerer();
+        for(int i = 1;i <= 11; i++)
+        {
+            sorcerer.Magic();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
